e311050510ea7689c091bd031accfbfd36304b94
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-07 19:50:44 -0500

    user input w3schools

---
+++ w3schools/35-user-input/demo563.py
@@ -0,0 +1,4 @@
+# W3Schools, Python User Input
+# Python 3.6 uses the input() method
+username = input("Enter username: ")
+print("Username is: " + username)
---
+++ w3schools/35-user-input/demo564.py
@@ -0,0 +1,4 @@
+# W3Schools, Python User Input
+# Python 2.7 uses the raw_input() method
+username = raw_input("Enter username: ")
+print("Username is: " + username)

b3d3ac4d92ac0d423b09fd9fbfe9f9402d92a955
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-18 16:45:07 -0500

    ML normal data distribution w3schools

---
+++ w3schools/39-machine-learning/05-normal-data-distribution/demo747.py
@@ -0,0 +1,8 @@
+# W3Schools, Python Machine Learning, Normal Data Distribution
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 100000)
+
+plt.hist(x, 100)
+plt.show()

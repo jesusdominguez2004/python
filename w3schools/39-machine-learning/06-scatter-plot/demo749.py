daecaeca465fc5eb2f4709ad3cc0e8c6839cb25d
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-18 16:45:23 -0500

    ML scatter plot w3schools

---
+++ w3schools/39-machine-learning/06-scatter-plot/demo749.py
@@ -0,0 +1,9 @@
+# W3Schools, Python Machine Learning, Scatter Plot
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()

fb672f11dd25c69b6cd4d32c5be9f0b8dee9bf0f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-19 18:20:35 -0500

    ML linear regression w3schools

---
+++ w3schools/39-machine-learning/07-linear-regression/demo755.py
@@ -0,0 +1,11 @@
+# W3Schools, Python Machine Learning, Linear Regression
+from scipy import stats
+
+# An example where linear regression 
+# would not be the best method to predict future values.
+x = [89, 43, 36, 36, 95, 10, 66, 34, 38, 20, 26, 29, 48, 64, 6, 5, 36, 66, 72, 40]
+y = [21, 46, 3, 35, 67, 95, 53, 72, 58, 10, 26, 34, 90, 33, 38, 20, 56, 2, 47, 15]
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+print(r) #  0.013 indicates a very bad relationship

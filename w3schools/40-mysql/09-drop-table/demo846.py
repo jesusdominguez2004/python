53c21412ccc3a76b4acc095b4eeed0c82c2aeba0
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-24 17:26:32 -0500

    mysql drop table w3schools

---
+++ w3schools/40-mysql/09-drop-table/demo846.py
@@ -0,0 +1,16 @@
+# W3Schools, Python MySQL, Drop Table
+import mysql.connector
+import mysql_credentials as mysqlc
+
+mydb = mysql.connector.connect(
+    host = mysqlc.host,
+    user = mysqlc.username,
+    password = mysqlc.password,
+    database = "mydatabase",
+)
+
+mycursor = mydb.cursor()
+
+sql = "DROP TABLE IF EXISTS customers"
+
+mycursor.execute(sql)

915b8d952f829e5bf1bfb11573b7f49f8f5cf4fa
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-24 17:23:52 -0500

    mysql delete w3schools

---
+++ w3schools/40-mysql/08-delete/demo843.py
@@ -0,0 +1,20 @@
+# W3Schools, Python MySQL, Delete
+import mysql.connector
+import mysql_credentials as mysqlc
+
+mydb = mysql.connector.connect(
+    host = mysqlc.host,
+    user = mysqlc.username,
+    password = mysqlc.password,
+    database = "mydatabase",
+)
+
+mycursor = mydb.cursor()
+
+sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
+
+mycursor.execute(sql)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
---
+++ w3schools/40-mysql/08-delete/demo844.py
@@ -0,0 +1,21 @@
+# W3Schools, Python MySQL, Delete
+import mysql.connector
+import mysql_credentials as mysqlc
+
+mydb = mysql.connector.connect(
+    host = mysqlc.host,
+    user = mysqlc.username,
+    password = mysqlc.password,
+    database = "mydatabase",
+)
+
+mycursor = mydb.cursor()
+
+sql = "DELETE FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2", )
+
+mycursor.execute(sql, adr)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")

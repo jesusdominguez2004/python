bedd3f58fcdbc7154d2e8a2da7e82be88a1f0333
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-24 16:51:10 -0500

    mysql insert w3schools

---
+++ w3schools/40-mysql/04-insert/demo833.py
@@ -0,0 +1,35 @@
+# W3Schools, Python MySQL, Insert
+import mysql.connector
+import mysql_credentials as mysqlc
+
+mydb = mysql.connector.connect(
+    host = mysqlc.host,
+    user = mysqlc.username,
+    password = mysqlc.password,
+    database = "mydatabase",
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+    ('Peter', 'Lowstreet 4'),
+    ('Amy', 'Apple st 652'),
+    ('Hannah', 'Mountain 21'),
+    ('Michael', 'Valley 345'),
+    ('Sandy', 'Ocean blvd 2'),
+    ('Betty', 'Green Grass 1'),
+    ('Richard', 'Sky st 331'),
+    ('Susan', 'One way 98'),
+    ('Vicky', 'Yellow Garden 2'),
+    ('Ben', 'Park Lane 38'),
+    ('William', 'Central st 954'),
+    ('Chuck', 'Main Road 989'),
+    ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "was inserted.")
---
+++ w3schools/40-mysql/04-insert/demo834.py
@@ -0,0 +1,20 @@
+# W3Schools, Python MySQL, Insert
+import mysql.connector
+import mysql_credentials as mysqlc
+
+mydb = mysql.connector.connect(
+    host = mysqlc.host,
+    user = mysqlc.username,
+    password = mysqlc.password,
+    database = "mydatabase",
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("Michelle", "Blue Village")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print("1 record inserted, ID:", mycursor.lastrowid)

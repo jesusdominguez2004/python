25dd8b5466b9246ee105feaa32cfcc79adb58c7c
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-24 17:18:19 -0500

    mysql order by w3schools

---
+++ w3schools/40-mysql/07-order-by/demo841.py
@@ -0,0 +1,21 @@
+# W3Schools, Python MySQL, Order by
+import mysql.connector
+import mysql_credentials as mysqlc
+
+mydb = mysql.connector.connect(
+    host = mysqlc.host,
+    user = mysqlc.username,
+    password = mysqlc.password,
+    database = "mydatabase",
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)

bf0e83836ec91347b5278cdb9db277f34af762c9
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-26 19:28:32 -0500

    mongodb drop collection w3schools

---
+++ w3schools/41-mongodb/09-drop-collection/demo880.py
@@ -0,0 +1,9 @@
+# W3Schools, Python MongoDB, Drop Collection
+import pymongo
+import mongodb_credentials as mdbc
+
+myclient = pymongo.MongoClient(mdbc.connection_string)
+mydb = myclient['mydatabase']
+mycol = mydb["customers"]
+
+mycol.drop()

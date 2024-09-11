08d76fd342684eb6ad8a9f67870e3eb0e4e7c45f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-09-10 20:54:34 -0500

    Python MCM 1.1

--- pruebas/mcm/README.md
+++ pruebas/mcm/README.md
@@ -2,11 +2,16 @@
 
 Versión 1.0:
 - Leer dos números del usuario
-- Leer cantidad de múltiplos de consula
+- Leer cantidad de múltiplos por consultar
 - Encontrar múltiplos del número01 del usuario
 - Encontrar múltiplos del número02 del usuario
 - Encontrar múltiplos comunes entre número1 y número2
 - Encontrar MCM (Número menor entre los múltiplos comunes)
+
+Versión 1.1:
+- Validar si lista múltiplos comunes no está vacía
+- Validar que números de usuario sean dígitos
+- Validar que cantidad múltiplos por consular sea un dígito
 
 --- 
 
--- pruebas/mcm/mcm.py
+++ pruebas/mcm/mcm.py
@@ -1,28 +1,45 @@
 # Python MCM (Mínimo Común Múltiplo)
 
 # 1. Leer números usuario
-numero01 = int(input("Ingrese un primer número: "))
-numero02 = int(input("Ingrese un segundo número: "))
-cantidad_multiplos = int(input("Ingrese cantidad de múltiplos de consulta: "))
+numero01 = input("Ingrese un primer número: ")
+numero02 = input("Ingrese un segundo número: ")
+cantidad_multiplos = input("Ingrese cantidad de múltiplos de consulta: ")
 
-# 2. Obtener multiplos del usuario
-numero01_multiplos = []
-numero02_multiplos = []
+if numero01.isdigit() and numero02.isdigit() and cantidad_multiplos.isdigit():
+    numero01 = int(numero01)
+    numero02 = int(numero02)
+    cantidad_multiplos = int(cantidad_multiplos)
 
-for x in range(cantidad_multiplos):
-    numero01_multiplo = numero01 * (x + 1)
-    numero02_multiplo = numero02 * (x + 1)
-    numero01_multiplos.append(numero01_multiplo)
-    numero02_multiplos.append(numero02_multiplo)
+    # 2. Obtener multiplos del usuario
+    numero01_multiplos = []
+    numero02_multiplos = []
 
-# 3. Encontrar multiplos comunes
-multiplos_comunes = []
-for multiplo in numero01_multiplos:
-    if multiplo in numero02_multiplos:
-        multiplos_comunes.append(multiplo)
+    for x in range(cantidad_multiplos):
+        numero01_multiplo = numero01 * (x + 1)
+        numero02_multiplo = numero02 * (x + 1)
+        numero01_multiplos.append(numero01_multiplo)
+        numero02_multiplos.append(numero02_multiplo)
 
-# 4. Calcular el menor múltiplo común
-mcm = min(multiplos_comunes)
+    # 3. Encontrar multiplos comunes
+    multiplos_comunes = []
+    for multiplo in numero01_multiplos:
+        if multiplo in numero02_multiplos:
+            multiplos_comunes.append(multiplo)
+
+    # 4. Calcular el menor múltiplo común
+    if len(multiplos_comunes) > 0:
+        mcm = min(multiplos_comunes)
+    else:
+        mcm = None
+        multiplos_comunes = None
+else:
+    numero01 = None
+    numero02 = None
+    cantidad_multiplos = None
+    numero01_multiplos = None
+    numero02_multiplos = None
+    multiplos_comunes = None
+    mcm = None
 
 # 5. Mostrar resultados en consola
 print(f"\nCantidad múltiplos: {cantidad_multiplos}")
